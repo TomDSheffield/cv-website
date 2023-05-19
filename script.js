67380c384fe3cc687963721a888dc1d3505d92bf
TomDSheffield <123513626+TomDSheffield@users.noreply.github.com>
2023-05-19 09:58:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function yourJavaScriptCode() {
+  // Your JavaScript code here
+  alert("Hello, world!");
+}
+In this example, the JavaScript code is placed in an external file called script.js. The HTML file includes this file using the <script> tag with the src attribute, which specifies the path to the JavaScript file. The yourJavaScriptCode() function is then called when the "Click me" link is clicked.
+
+Both inline and external JavaScript can be used to link HTML to JavaScript code. The choice depends on the complexity of your code and organization preferences.
+
+
+
+
+
+
