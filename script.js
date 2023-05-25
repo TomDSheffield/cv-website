933762599d9fe03e7a3911ac0a629c12380d9872
TomDSheffield <123513626+TomDSheffield@users.noreply.github.com>
2023-05-25 09:08:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function yourJavaScriptCode() {
   // Your JavaScript code here
-  alert("Hello, world!");
+  console.log("Hello, world!");
 }
 In this example, the JavaScript code is placed in an external file called script.js. The HTML file includes this file using the <script> tag with the src attribute, which specifies the path to the JavaScript file. The yourJavaScriptCode() function is then called when the "Click me" link is clicked.
 
